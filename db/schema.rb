b8e948085ec23a7d210a8ab6b1c14f632527f969
Mark Mendoza <mendoza.mark.a@gmail.com>
2015-02-22 09:29:42 -0800

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222101836) do
+ActiveRecord::Schema.define(version: 20150222102346) do
 
   create_table "offers", force: :cascade do |t|
     t.datetime "created_at", null: false
