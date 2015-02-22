e9814927653f8aebf5bed095624272fa1938e3bf
Mark Mendoza <mendoza.mark.a@gmail.com>
2015-02-22 00:28:12 -0800

    window dressing
    window dressing

--- studytime/app/controllers/requests_controller.rb
+++ studytime/app/controllers/requests_controller.rb
@@ -2,13 +2,8 @@ class RequestsController < ApplicationController
     def index
         @requests = Request.all
     end
-    def show
-        @request = Request.find(params[:id])
-    end
     def new
         @request = Request.new
-    end
-    def edit
     end
     def create
         @request = Request.new(params[:request].permit(:requester,:email)) 
@@ -17,8 +12,6 @@ class RequestsController < ApplicationController
         else
            render :action => :new 
         end
-    end
-    def update
     end
     def destroy
         @request = Request.find(params[:id])
