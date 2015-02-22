f90f344c87202ae0c7bafd01dc3e783a44b9cf1a
Mark Mendoza <mendoza.mark.a@gmail.com>
2015-02-21 21:23:49 -0800

    Getting css to work

--- studytime/db/schema.rb
+++ studytime/db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222015734) do
+ActiveRecord::Schema.define(version: 20150222024355) do
 
   create_table "requests", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "roomNum"
     t.string   "requester"
+    t.string   "email"
   end
 
   create_table "sessions", force: :cascade do |t|
