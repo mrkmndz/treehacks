10cd0a23de701ee178ccf22bf9f2a89e0b8adedc
Mark Mendoza <mendoza.mark.a@gmail.com>
2015-02-22 01:17:06 -0800

    some generation

--- studytime/db/schema.rb
+++ studytime/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150222024355) do
+ActiveRecord::Schema.define(version: 20150222091015) do
+
+  create_table "offers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "requests", force: :cascade do |t|
     t.datetime "created_at", null: false
