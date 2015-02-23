0b20af8774cbe793204670fcbe9e1cd084bca360
Mark Mendoza <mendoza.mark.a@gmail.com>
2015-02-22 20:17:17 -0800

    starting offer handler

--- app/controllers/offer_controller.rb
+++ app/controllers/offer_controller.rb
@@ -1,2 +1,24 @@
 class OfferController < ApplicationController
+    def index
+        @offers = Offer.all
+    end
+    def new
+        @offer = Request.new
+    end
+    def create
+        @offer = Request.new(params[:request].permit(:room,:offerer)) 
+        if @offer.save then
+            redirect_to(:action => :index)
+        else
+           render :action => :new 
+        end
+    end
+    def destroy
+        @request = Request.find(params[:id])
+        if @request.destroy then
+            redirect_to(:action => :index)
+        else
+            render :action => :show
+        end
+    end
 end
