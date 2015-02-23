43410bed7a34ec5bb4678824ac416d083b3aa8d6
Mark Mendoza <mendoza.mark.a@gmail.com>
2015-02-22 23:48:11 -0800

    Getting imagemapster to work

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,15 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( when.js )
+Rails.application.config.assets.precompile += %w( core.js )
+Rails.application.config.assets.precompile += %w( graphics.js )
+Rails.application.config.assets.precompile += %w( mapimage.js )
+Rails.application.config.assets.precompile += %w( mapdata.js )
+Rails.application.config.assets.precompile += %w( areadata.js )
+Rails.application.config.assets.precompile += %w( areacorners.js )        
+Rails.application.config.assets.precompile += %w( scale.js )
+Rails.application.config.assets.precompile += %w( tooltip.js ) 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
